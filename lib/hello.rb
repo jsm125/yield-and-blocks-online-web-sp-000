d083ca50711bd912f0931baeee3ff7c218ee923c
Matt Im <matthew.seiim@gmail.com>
2020-04-29 21:49:25 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,10 @@
 def hello_t(array)
-
+  [array].each do |name|
+  puts "#{name}"
+  end
 end
 
 
-    hello_t(["Tim", "Tom", "Jim"]) do |name|
-      if name.start_with?("T")
-        puts "Hi, #{name}"
-      end
-    end
+
 
 
